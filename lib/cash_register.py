20998f861f5854927c885e111cfb40ab238cdc4a
chikealvan <nwadoziechikealvan@gmail.com>
2023-10-12 05:59:32 +0100

    first commit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.last_transaction = 0  # To keep track of the last transaction amount
+
+    def add_item(self, item_name, price, quantity=1):
+        self.total += price * quantity
+        for _ in range(quantity):
+            self.items.append(item_name)
+        self.last_transaction = price * quantity  # Update last transaction
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * self.discount / 100.0
+            self.total -= discount_amount
+            return f"Discounted total: ${self.total}"
+        else:
+            return "No discount to apply"
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item = self.items.pop()
+            last_price = self.last_transaction
+            self.total -= last_price
+            return f"Removed last transaction: {last_item} (${last_price})"
+        else:
+            return "No transactions to void"
